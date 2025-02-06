54eaa7921787b77d715ba879dabcd8477da11fd0
CharlesWithC <me@charlws.com>
2025-02-06 18:54:47 +0000

    lecture 250206

--- week-5/struct.c
+++ week-5/struct.c
@@ -9,10 +9,13 @@ typedef struct Person_struct {
 
 typedef struct ListNode_struct {
     Person p;
+    struct ListNode_struct *previous;
     struct ListNode_struct *next;
 } ListNode;
 
-ListNode *NewListNode(ListNode *curNode, char name[], int age) {
+// takes in current node address, create new node and list current node to new node
+// return new node address
+ListNode *NewListNode(ListNode *curNode, const char name[], const int age) {
     ListNode *newNode = NULL;
     newNode = (ListNode *)calloc(1, sizeof(ListNode));
     if (newNode == NULL) {
@@ -25,20 +28,71 @@ ListNode *NewListNode(ListNode *curNode, char name[], int age) {
     }
     strcpy((newNode->p).name, name);
     (newNode->p).age = age;
+    newNode->previous = curNode;
     newNode->next = NULL;
 
     return newNode;
 }
 
-int main() {
-    ListNode *parker = NewListNode(NULL, "Parker", 50);
-    ListNode *thomas = NewListNode(parker, "Thomas", 100);
-    ListNode *kathleen = NewListNode(thomas, "Kathleen", 60);
-
-    ListNode *curNode = parker;
-    while (curNode != NULL) {
-        printf("%s, %d\n", (*curNode).p.name, (*curNode).p.age);
-        curNode = (*curNode).next;
+// takes in name
+// return the list node matching the name
+ListNode *FindListNode(ListNode *head, const char name[]) {
+    ListNode *cur = head;
+    while (cur != NULL) {
+        if (strcmp((*cur).p.name, name) == 0) {
+            return cur;
+        }
+        cur = cur->next;
     }
+    return NULL;
+}
+
+// takes in node to delete
+// relink previous node
+void DeleteListNode(ListNode *node) {
+    if (node->previous != NULL) {
+        (node->previous)->next = node->next;
+    }
+    if (node->next != NULL) {
+        (node->next)->previous = node->previous;
+    }
+    free(node);
+}
+
+int main() {
+    const char name[10][10] = {"Parker", "Thomas", "Kathleen", "Rawd", "Purva", "Paco"};
+    const int age[10] = {20, 30, 40, 30, 30, 50};
+    const int size = 6;
+
+    ListNode *head = NewListNode(NULL, "HEAD", -1);
+
+    ListNode *cur = head;
+    for (int i = 1; i < size; i++) {
+        cur = NewListNode(cur, name[i], age[i]);
+    }
+
+    printf("Initial list\n");
+    cur = head->next;
+    while (cur != NULL) {
+        printf("%s, %d\n", (*cur).p.name, (*cur).p.age);
+        cur = (*cur).next;
+    }
+
+    printf("\nFind purva\n");
+    ListNode *purva = FindListNode(head, "Purva");
+    printf("Purva is at %p\n", purva);
+    printf("%s, %d\n", purva->p.name, purva->p.age);
+
+    printf("\nNew list after deleting purva\n");
+    DeleteListNode(purva);
+    cur = head->next;
+    while (cur != NULL) {
+        printf("%s, %d\n", (*cur).p.name, (*cur).p.age);
+        cur = (*cur).next;
+    }
+
+    printf("\nTry to get purva again?\n");
+    printf("%s, %d\n", purva->p.name, purva->p.age);
+
     return 0;
 }
