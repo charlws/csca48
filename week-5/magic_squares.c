17e85d633bbeae7f0374406cec581f44c055507b
CharlesWithC <me@charlws.com>
2025-02-09 19:11:28 +0000

    improve magic square solution

--- week-5/magic_squares.c
+++ week-5/magic_squares.c
@@ -6,6 +6,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int validateMagicSquare(int square[6][6], int sum) {
+    int ok = 1;
+    for (int i = 0; i < 6; i++) {
+        int local_sum = 0;
+        for (int j = 0; j < 6; j++) local_sum = local_sum + square[i][j];
+        if (local_sum > sum) return 2;
+        if (local_sum != sum) ok = 0;
+    }
+    for (int j = 0; j < 6; j++) {
+        int local_sum = 0;
+        for (int i = 0; i < 6; i++) local_sum = local_sum + square[i][j];
+        if (local_sum > sum) return 2;
+        if (local_sum != sum) ok = 0;
+    }
+    return ok;
+}
+
+int dfs(int square[6][6], int numbers_to_try[36], int try_size, int sum) {
+    // do not question why I can write a dfs at this stage
+    // dfs is my favoriate when I was doing OI (Olympics in Informatics) in middle school
+    // bcz I usually cannot figure out some fancy math-ish solutions (unless it's a graph)
+
+    // (C) CharlesWithC
+
+    int validation_result = validateMagicSquare(square, sum);
+    if (validation_result == 1) return 1;
+    if (validation_result == 2) return 0;  // already bigger than sum / return early
+    // the second optimization does make things much faster
+
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            if (square[i][j] == -1) {
+                for (int k = 0; k < try_size; k++) {
+                    int to_try = numbers_to_try[k];
+                    if (to_try == -1) continue;  // skip used numbers
+                    square[i][j] = to_try;
+                    numbers_to_try[k] = -1;                                    // mark as used
+                    if (dfs(square, numbers_to_try, try_size, sum)) return 1;  // return early if successful
+                    // revert changes
+                    square[i][j] = -1;
+                    numbers_to_try[k] = to_try;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 void solveMagicSquare(int square[6][6]) {
     // This function receives an array of size 6x6
     // that corresponds to a magic square.
@@ -68,6 +116,8 @@ void solveMagicSquare(int square[6][6]) {
         if (ok == 1) sum = col_sum;
     }
 
+    if (sum == -1) sum = 111;  // f*** it
+
     // PART-II: we iterate through the matrix for 100 times, trying to solve single unknowns
     // for each iteration, we find the row/column with single unknown and solve it
     // if there's multiple unknowns, we don't solve it
@@ -122,11 +172,24 @@ void solveMagicSquare(int square[6][6]) {
     }
 
     // PART-III: if there's still unknowns
-    // then just f*** it
     // I'm not writing a guassian elimination in an a48 "exercise"
     // (I can consider about it if it's an "assignment")
+    // since the number must be 1-36
+    // we try a dfs
     if (num_unknowns > 0) {
-        printf("Paco, I hate you! (lol)\n");
+        int unused_numbers[37];  // bool 0/1
+        for (int i = 0; i <= 36; i++) unused_numbers[i] = 1;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (square[i][j] != -1) unused_numbers[square[i][j]] = 0;
+            }
+        }
+
+        int numbers_to_try[36], try_size = 0;
+        for (int i = 1; i <= 36; i++)
+            if (unused_numbers[i]) numbers_to_try[try_size++] = i;
+
+        dfs(square, numbers_to_try, try_size, sum);
     }
 }
 
@@ -164,8 +227,8 @@ int main() {
                        {10, 11, 18, -1, 26, 27}, {13, -1, 36, 33, 5, 8}, {14, 15, 34, 35, 6, -1}};
     // int solution[6][6] = {{32, 29, 4, 1, 24, 21},   {30, 31, 2, 3, 22, 23}, {12, 9, 17, 20, 28, 25},
     //                     {10, 11, 18, 19, 26, 27}, {13, 16, 36, 33, 5, 8}, {14, 15, 34, 35, 6, 7}};
-    // int magic[6][6] = {{32, 29, 4, 1, -1, -1},   {30, -1, 2, 3, 22, 23}, {12, 9, 17, -1, 28, 25},
-    //                    {-1, 11, 18, 19, -1, 27}, {13, -1, 36, 33, 5, 8}, {14, -1, 34, -1, 6, -1}};
+    // int magic[6][6] = {{32, -1, 4, 1, -1, -1},   {30, -1, 2, 3, -1, 23}, {12, 9, 17, -1, 28, 25},
+    //                    {10, -1, 18, 19, -1, 27}, {13, 16, -1, 33, 5, 8}, {14, 15, 34, -1, 6, -1}};
 
     printMagicSquare(magic);
     printf("Solving Magic Square!\n");
