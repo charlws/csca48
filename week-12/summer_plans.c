c32d031265e7272ad3e0b12635dd666e017426a0
CharlesWithC <me@charlws.com>
2025-04-05 20:14:46 +0000

    assignment-3

---
+++ week-12/summer_plans.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("This summer, Charles is planning to:\n    Take a few summer courses, continue part-time tech jobs, and bike/hike in the nice trails (or even head to Bannf again).\n");
+    return 0;
+}
